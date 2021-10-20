f726693eec2bf4401d001b8f2d06fabf89640fde
vpetridis <vpetridis@modulus.gr>
2021-10-20 23:11:08 +0300

    ADDED title transition animation

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import Section from "./components/Section";
 function App() {
   return (
     <div className="App">
-      <Section sectionArea="music-container" title="music"></Section>
-      <Section sectionArea="teach-container" title="teach"></Section>
-      <Section sectionArea="dev-container" title="dev"></Section>
+      <Section sectionArea="music-container" title="music">
+        I am a music composer
+      </Section>
+      <Section sectionArea="teach-container" title="teach">
+        I am a former teacher
+      </Section>
+      <Section sectionArea="dev-container" title="dev">
+        I am a front-end developer
+      </Section>
     </div>
   );
 }
--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -2,38 +2,40 @@ import React, { useEffect, useRef, useState } from "react";
 import "./_section.css";
 
 const Section = ({ children, sectionArea, title }) => {
-    const ref = useRef();
-    const [mouseEntered, setMouseEntered] = useState();
-    const [mouseLeft, setMouseLeft] = useState();
+  const ref = useRef();
+  const [mouseEntered, setMouseEntered] = useState();
+  const [mouseLeft, setMouseLeft] = useState();
 
-    useEffect(() => {
-        const el = ref.current;
-        el.addEventListener("mouseenter", () => {
-            setMouseEntered(true);
-            setMouseLeft(false);
-        });
-        el.addEventListener("mouseleave", () => {
-            setMouseEntered(false);
-            setMouseLeft(true);
-        });
-        return () => {
-            el.removeEventListener("mouseenter", setMouseEntered(false));
-            el.removeEventListener("mouseleave", setMouseEntered(false));
-        };
-    }, [ref]);
+  useEffect(() => {
+    const el = ref.current;
+    el.addEventListener("mouseenter", () => {
+      setMouseEntered(true);
+      setMouseLeft(false);
+    });
+    el.addEventListener("mouseleave", () => {
+      setMouseEntered(false);
+      setMouseLeft(true);
+    });
+    return () => {
+      el.removeEventListener("mouseenter", setMouseEntered(false));
+      el.removeEventListener("mouseleave", setMouseEntered(false));
+    };
+  }, [ref]);
 
-    return (
-        <div
-            ref={ref}
-            className={`section-container ${sectionArea} ${
-                mouseEntered && "mouse-entered expand-section"
-            }`}
-            style={{ flexBasis: mouseLeft && "100%" }}
-        >
-            <h5 className="section--title">{title}</h5>
-            {children}
-        </div>
-    );
+  return (
+    <div
+      ref={ref}
+      className={`section-container ${sectionArea} ${
+        mouseEntered && "mouse-entered expand-section"
+      }`}
+      style={{ flexBasis: mouseLeft && "100%" }}
+    >
+      <h5 className={`section--title ${mouseEntered && "title-move"}`}>
+        {title}
+      </h5>
+      {children}
+    </div>
+  );
 };
 
 export default Section;
