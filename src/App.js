f75ac8bbe0f9f37cd3cd19c60cacf7c34b81f537
vpetridis <vpetridis@modulus.gr>
2021-10-20 22:54:31 +0300

    ADDED sections on App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Section sectionArea="music-container" title="music"></Section>
+      <Section sectionArea="teach-container" title="teach"></Section>
+      <Section sectionArea="dev-container" title="dev"></Section>
     </div>
   );
 }
